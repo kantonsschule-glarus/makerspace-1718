2943e4570c5bf3787bbeb198c44deff11e269ee5
mnemonia <birkeland@gmx.net>
2017-09-02 14:22:10 +0200

    First commit of the robot pilot app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # makerspace-1718
+Here you find some of the robots, apps and things we create in the makerspace season 2017/18.
---
+++ robot-pilot-app/src/pages/home/home.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public startDirectionX: number = 375;
+  public startDirectionY: number = 200;
+  public startRotationY: number = 200;
+  public directionX: number = 375;
+  public directionY: number = 125;
+  public roationY: number = 125;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  getDirectionXY(e){
+    this.directionX = e.srcEvent.offsetX
+    this.directionY = e.srcEvent.offsetY
+    if(e.isFinal){
+      this.directionX = 375
+      this.directionY = 125
+    }
+  }
+
+  getRoationY(e){
+    this.roationY = e.srcEvent.offsetY
+    /*if(this.roationY > 250){
+      this.roationY = 250
+    }
+
+    else if(this.roationY < 0){
+      this.roationY = 0
+    }*/
+    if(e.isFinal){
+      this.roationY = 125
+    }
+  }
+}
