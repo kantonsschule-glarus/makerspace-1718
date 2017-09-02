3361b41ae3751adfb1c6877c00c412d8e52e00da
mnemonia <birkeland@gmx.net>
2017-09-02 19:42:21 +0200

    Added the HttpModule

--- robot-pilot-app/src/app/app.module.ts
+++ robot-pilot-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -15,6 +16,7 @@ import { RobotCommunicationService } from '../services/robot-communication-servi
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
