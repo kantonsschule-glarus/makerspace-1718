a352f99c8e495ad5c7c4c50031d521bf88735f77
mnemonia <birkeland@gmx.net>
2017-09-02 18:10:53 +0200

    Created the RobotCommunicationService. This service reads and writes data from and to the robot via HTTP protocol.

--- robot-pilot-app/src/app/app.module.ts
+++ robot-pilot-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { RobotCommunicationService } from '../services/robot-communication-service/robot-communication-service';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RobotCommunicationService
   ]
 })
 export class AppModule {}
