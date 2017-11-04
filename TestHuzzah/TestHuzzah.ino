1e9ddf9dead0ac1851e88fb6b719361b56f66a51
Christian Hürlimann <huerlimann@users.noreply.github.com>
2017-11-04 23:43:23 +0100

    Added HuzzahTest
    The added program is used to test the functionality of a Feather Huzzah robot with a motor shield and two motor attached to it

---
+++ TestHuzzah/TestHuzzah.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+Adafruit_DCMotor *motorOne = AFMS.getMotor(1);
+Adafruit_DCMotor *motorTwo = AFMS.getMotor(2);
+
+
+void setup() {
+  Serial.begin(9600);
+
+  AFMS.begin();
+}
+
+void loop() {
+  motorOne -> run(FORWARD);
+  motorTwo -> run(FORWARD);
+  for(int i = 0; i < 256; i++){
+    motorOne -> setSpeed(i);
+    motorTwo -> setSpeed(i);
+    delay(10);
+  }
+  
+  motorOne -> run(BACKWARD);
+  motorTwo -> run(BACKWARD);
+  for(int i = 0; i < 256; i++){
+    motorOne -> setSpeed(i);
+    motorTwo -> setSpeed(i);
+    delay(10);
+  }
+  
+  motorOne -> run(FORWARD);
+  motorTwo -> run(BACKWARD);
+  for(int i = 0; i < 256; i++){
+    motorOne -> setSpeed(i);
+    motorTwo -> setSpeed(i);
+    delay(10);
+  }
+
+  motorOne -> run(BACKWARD);
+  motorTwo -> run(FORWARD);
+  for(int i = 0; i < 256; i++){
+    motorOne -> setSpeed(i);
+    motorTwo -> setSpeed(i);
+    delay(10);
+  }
+}
